f83a89e7d1d2398ac56ae935e0eba08c77922997
Petr Kalinin <petr@kalinin.nnov.ru>
2019-10-19 21:01:09 +0300

    Added temperature and humidity sensors

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3 -u
 import time
 from LightSetter import LightSetter
+from SHT20 import SHT20
 
 light_setter = LightSetter()
+sht20 = SHT20()
 
 while True:
     light_setter()
+    sht20()
     time.sleep(60)
