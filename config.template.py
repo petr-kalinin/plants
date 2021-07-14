5692ca4e381c93c108bf2a89616eabacd0c6e3cf
Petr Kalinin <petr@kalinin.nnov.ru>
2021-07-14 13:08:17 +0300

    Move pump parameters to config

--- config.template.py
+++ config.template.py
@@ -17,3 +17,11 @@ joystick = # False
 heater_t_max = # 20
 dacha_pump = # False
 graphite_attempts = # 5
+
+class PUMP_PARAMETERS:
+    AFTER_WATER_DELAY = # 1 * 24 * 60 * 60
+    PUMP_INIT_TIME = # 3
+    PUMP_POST_INIT_TIME = # 3
+    PUMP_ACTIVE_TIME = # 20
+    PUMP_WAIT_TIME = # 60 * 60
+    PUMP_ITERATIONS = # 30
