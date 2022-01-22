3d7e2973161eb29f54982b8fa1f64e2f9c3a5769
Petr Kalinin <petr@kalinin.nnov.ru>
2022-01-22 21:53:27 +0300

    Fix LightSunController

--- LightSunController.py
+++ LightSunController.py
@@ -17,10 +17,9 @@ class LightSunController:
         delta = datetime.datetime.now() - self.sun.sunset()
         logging.info("daylength={} min, needed_len={} min".format(daylength/60, needed_len/60))
         value = False
-        if needed_len > daylength:
-            last_delta = needed_len - daylength
-            logging.info("last_delta={} min, now_delta={}".format(last_delta/60, delta.seconds/60))
-            value = delta > datetime.timedelta(hours=-1) and delta < datetime.timedelta(seconds=last_delta)
+        last_delta = needed_len - daylength
+        logging.info("last_delta={} min, now_delta={}".format(last_delta/60, delta.seconds/60))
+        value = delta > datetime.timedelta(hours=-1) and delta < datetime.timedelta(seconds=last_delta)
         await self.graphite.send("light_sun", int(value))
         values = []
         for i in range(3):
