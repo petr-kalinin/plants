1cb9d2c684cd145eb54a0bba0deda3ce59247493
Petr Kalinin <petr@kalinin.nnov.ru>
2022-07-24 14:41:14 +0300

    Adjust dacha pump constants

--- DachaPumpController.py
+++ DachaPumpController.py
@@ -4,10 +4,10 @@ import math
 
 # Note that distance is reversed: this is the distance from sensor to water,
 # not the distance from water to bottom.
-DISTANCE_FOR_OFF = 15
+DISTANCE_FOR_OFF = 5
 DISTANCE_FOR_ON = 40
-MAX_PUMP_TIME = datetime.timedelta(minutes=5)
-PERIOD_TIME = datetime.timedelta(minutes=15)
+MAX_PUMP_TIME = datetime.timedelta(minutes=15)
+PERIOD_TIME = datetime.timedelta(minutes=60)
 
 class DachaPumpController:
     def __init__(self, distance, pump, graphite):
@@ -29,9 +29,10 @@ class DachaPumpController:
             self.start_time = None
         if self.start_time and now - self.start_time < MAX_PUMP_TIME:
             await self.pump.start()
+            await self.graphite.send("pump", 1)
         else:
             await self.pump.stop()
-        await self.graphite.send("pump", 1 if self.start_time else 0)
+            await self.graphite.send("pump", 0)
 
     def delay(self):
         return 1
