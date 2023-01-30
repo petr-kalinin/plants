6a6b08c8dc03185775d356ea9e0e926dc518d7a3
Petr Kalinin <petr@kalinin.nnov.ru>
2023-01-30 21:34:10 +0300

    Monitor algo tab

--- ProcMonitor.py
+++ ProcMonitor.py
@@ -1,7 +1,9 @@
 import asyncio
+import logging
 import psutil
 import subprocess
 import os
+from urllib.parse import urlparse
 
 
 class ProcMonitor:
@@ -16,6 +18,33 @@ class ProcMonitor:
         except:
             name = "unknown"
         await self.graphite.send('procname.' + name, 1)
+        if name == "firefox":
+            await self.send_tab("firefox")
+        elif name == "yandex_browser":
+            await self.send_tab("chrome/chromium")
+
+    async def send_tab(self, browser):
+        url = self.get_tab(browser)
+        logging.info("Current url: {}".format(url))
+        tab = self.get_domain(url)
+        await self.graphite.send('tab.' + tab, 1)
+
+    def get_tab(self, browser):
+        active_list = subprocess.check_output(["/home/petr/.local/bin/bt", "active"]).decode("utf-8").strip().split("\n")
+        active_id = None
+        for active_tab in active_list:
+            data = active_tab.split("\t")
+            if data[-1].strip() == browser:
+                active_id = data[0].strip()
+        all_list = subprocess.check_output(["/home/petr/.local/bin/bt", "list"]).decode("utf-8").strip().split("\n")
+        for tab in all_list:
+           data = tab.split("\t")
+           if data[0] == active_id:
+               return data[-1]
+        return "unknown"
+
+    def get_domain(self, url):
+        return urlparse(url).netloc.replace(".", "_")
 
     def delay(self):
         return self.graphite.delay()
