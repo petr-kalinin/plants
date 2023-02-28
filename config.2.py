807755ec6afa2ea4b941d7dfa3bd5119fe508984
Petr Kalinin <petr@kalinin.nnov.ru>
2023-02-28 18:14:44 +0300

    Added config.2.py

---
+++ config.2.py
@@ -0,0 +1,29 @@
+is_mock = False
+th_monitor = True
+graphite_instance = 2
+soils = 0
+distance = True
+pump = True
+lightness = []
+i2c = 1
+light = True
+light_sun = False
+level = True
+invert_level = False
+rtl433 = False
+ping = True
+heater = False
+display = False
+joystick = False
+heater_t_max = 20
+dacha_pump = False
+graphite_attempts = 5
+procmonitor = False
+
+class PUMP_PARAMETERS:
+    AFTER_WATER_DELAY = 3 * 24 * 60 * 60
+    PUMP_INIT_TIME = 3
+    PUMP_POST_INIT_TIME = 3
+    PUMP_ACTIVE_TIME = 5 * 60
+    PUMP_WAIT_TIME = 1 * 60
+    PUMP_ITERATIONS = 10
