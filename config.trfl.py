2103097d99f0e0a69a77b27bd7b00dd2facd41b3
Petr Kalinin <petr@kalinin.nnov.ru>
2024-03-10 17:14:20 +0300

    Add trfl config

---
+++ config.trfl.py
@@ -0,0 +1,29 @@
+is_mock = False
+th_monitor = True
+graphite_instance = "trfl"
+soils = 0
+distance = False
+pump = False
+lightness = []
+i2c = 0
+light = False
+light_sun = False
+level = False
+invert_level = True
+rtl433 = False
+ping = True
+heater = False
+display = False
+joystick = False
+heater_t_max = 20
+dacha_pump = False
+graphite_attempts = 5
+procmonitor = False
+
+class PUMP_PARAMETERS:
+    AFTER_WATER_DELAY = 1 * 24 * 60 * 60
+    PUMP_INIT_TIME = 3
+    PUMP_POST_INIT_TIME = 3
+    PUMP_ACTIVE_TIME = 20
+    PUMP_WAIT_TIME = 60 * 60
+    PUMP_ITERATIONS = 30
