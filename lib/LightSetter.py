3e4c09e46e504d7a3f1535715f8e62e6e9d05f9d
Petr Kalinin <petr@kalinin.nnov.ru>
2025-03-20 22:42:46 +0300

    Fix ports

--- lib/LightSetter.py
+++ lib/LightSetter.py
@@ -4,7 +4,7 @@ try:
     from pyA20.gpio import gpio
     from pyA20.gpio import port
 
-    leds = [port.PA6]
+    leds = [port.PA11]
 
     if not os.getegid() == 0:
         sys.exit('Script must be run as root')
