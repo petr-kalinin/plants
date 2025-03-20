4e8fc2d9cbaf7b425c9979535de4ba50629fadc5
Petr Kalinin <petr@kalinin.nnov.ru>
2025-03-20 22:40:25 +0300

    Fix

--- PumpController.py
+++ PumpController.py
@@ -29,9 +29,9 @@ class PumpController:
 
     async def __call__(self):
         if os.path.exists("pump_controller_lock" + self.id):
-            await self.graphite.send("pump" + id, -1)
+            await self.graphite.send("pump" + self.id, -1)
             return
-        await self.graphite.send("pump" + id, 0)
+        await self.graphite.send("pump" + self.id, 0)
 
         if await self.level() > 0:
             self.last_level_time = time.time()
@@ -54,17 +54,17 @@ class PumpController:
                 await self.pump.start()
                 start_time = time.time()
                 while await self.level() == 0 and time.time() < start_time + self.parameters.PUMP_ACTIVE_TIME:
-                    await self.graphite.send("pump" + id, 1)
+                    await self.graphite.send("pump" + self.id, 1)
                     await asyncio.sleep(1)
                 await self.pump.stop()
                 # make sure pump=1 makes it to Graphite
                 for i in range(4):
-                    await self.graphite.send("pump" + id, 1)
+                    await self.graphite.send("pump" + self.id, 1)
                     await asyncio.sleep(20)
                 start_time = time.time()
                 while time.time() < start_time + self.parameters.PUMP_WAIT_TIME:
                     await self.pump.stop()
-                    await self.graphite.send("pump" + id, 0)
+                    await self.graphite.send("pump" + self.id, 0)
                     await asyncio.sleep(10)
         finally:
             await self.pump.stop()
